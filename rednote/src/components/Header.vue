fc1bbb21f4e6493a4afcccf4ba17dc780a814433
deathcrush <reidvai1998@gmail.com>
2019-09-20 16:05:20 +0600

    added component skeleton

---
+++ rednote/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title>
+        <span>RED</span>
+        <span>note</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn depressed @click="navto({name: 'home'})">
+          <span>Home</span>
+          <v-icon right>mdi-home</v-icon>
+        </v-btn>
+        <v-btn depressed @click="navto({name: 'about'})">About</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>    
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    navto(route){
+      this.$router.push(route);
+    }
+  }
+}
+</script>
---
+++ rednote/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <Register /> -->
+    <Dash />
+  </div>
+</template>
+
+<script>
+// import Register from '../components/Register';
+import Dash from '../components/Dash';
+
+export default {
+  components: {
+    // Register,
+    Dash
+  },
+};
+</script>
