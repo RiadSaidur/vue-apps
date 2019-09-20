96948cb224af866f47ac8f6b9da1396bcb8758cf
deathcrush <reidvai1998@gmail.com>
2019-09-20 17:53:21 +0600

    added New Note component

---
+++ rednote/src/components/New.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>New Note</h2>
+    <v-form>
+      <v-textarea label='title' v-model="title"></v-textarea>
+      <v-textarea label='note' v-model="note"></v-textarea>
+      <v-btn @click='save'>Save</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      title: '',
+      note: ''
+    }
+  },
+  methods: {
+    save(){
+      this.title='';
+      this.note='';
+    }
+  }
+}
+</script>
