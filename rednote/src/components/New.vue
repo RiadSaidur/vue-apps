b6ae44a5b1c54b37576b31b4034b6de2f9b4adf4
deathcrush <reidvai1998@gmail.com>
2019-09-26 01:05:55 +0600

    edit and delete notes on demand

--- rednote/src/components/New.vue
+++ rednote/src/components/New.vue
@@ -34,15 +34,9 @@ export default {
         const size = temp.length;
         for(let i=0; i<size; i++){
           notes.push(temp[i]);
-          // console.log(`${i} : ${notes}`);
         }
-        // console.log(localStorage.notes);
-        // notes = [...notes];
         notes.push(newNote);
         localStorage.notes = JSON.stringify(notes);
-        // console.log('shite again', localStorage.notes);
-        // console.log(notes);
-        console.log(JSON.parse(localStorage.notes));
       } else{
         localStorage.id = id;
         const newNote = {
@@ -51,7 +45,6 @@ export default {
           note: this.note
         };
         localStorage.notes = JSON.stringify(newNote);
-        // console.log('shite again', localStorage.notes);
       }
       this.title = '';
       this.note = '';
--- rednote/src/views/Edit.vue
+++ rednote/src/views/Edit.vue
@@ -4,13 +4,72 @@
     <v-form>
       <v-textarea label='title' v-model="title"></v-textarea>
       <v-textarea label='note' v-model="note"></v-textarea>
-      <v-btn @click='save'>Save</v-btn>
+      <v-btn @click="save({name: 'home'})">Save</v-btn>
     </v-form>
+    <div class="dlt">
+      <v-btn @click="dlt({name: 'home'})">Delete Note</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props:['id']
+  props:['id'],
+  data(){
+    return{
+      index: '',
+      title: '',
+      note: ''
+    }
+  },
+  methods: {
+    save(route){
+      let data = JSON.parse(localStorage.notes);
+
+      data.splice(this.index, 1);
+
+      const newNote = {
+        id: this.id,
+        title: this.title,
+        note: this.note
+      };
+
+      data.push(newNote);
+      
+      localStorage.notes = JSON.stringify(data);
+      
+      this.$router.push(route);
+    },
+    dlt(route){
+      let data = JSON.parse(localStorage.notes);
+
+      data.splice(this.index, 1);
+
+      localStorage.notes = JSON.stringify(data);
+
+      this.$router.push(route);
+    }
+  },
+  mounted(){
+    let data = JSON.parse(localStorage.notes);
+
+    const length = data.length;
+    for(let i=0; i<length; i++){
+      if(data[i].id == this.id){
+        this.index = i;
+        this.title = data[i].title;
+        this.note =  data[i].note;
+        break;
+      }
+    }
+  }
 }
 </script>
+
+<style scoped>
+  .dlt{
+    padding: 1rem 0;
+  }
+</style> scoped>
+
+</style>
