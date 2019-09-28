aca29355ea1e19e8aab08d6c1b88d883d9a5b855
deathcrush <reidvai1998@gmail.com>
2019-09-28 18:18:47 +0600

    added temporary static content

--- nasa-api/src/components/PhotoOfDay.vue
+++ nasa-api/src/components/PhotoOfDay.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
-    <h2>An image will be shown here</h2>
+    <v-container>
+    
+    <router-link to="/">
+      <v-btn class="mb-2">
+        <v-icon left>mdi-home</v-icon>
+        <span>Back to Home</span>
+      </v-btn>
+    </router-link>
+
+    <v-card flat outlined class="mx-auto">
+
+      <v-img :src="image" class="ma-auto my-2" max-width="500"></v-img>
+      
+      <v-card-text>caption here</v-card-text>
+
+      <v-card-title>
+        image title
+      </v-card-title>
+
+      <v-card-text>
+        description
+      </v-card-text>
+
+      <v-card-action>
+        <v-btn>
+          more
+        </v-btn>
+      </v-card-action>
+
+    </v-card>
+
+    </v-container>
   </div>
 </template>
 
@@ -10,7 +41,7 @@ export default {
 
   data(){
     return{
-      //
+      image: `http://images-assets.nasa.gov/image/0302063/0302063~orig.jpg`
     }
   },
 
