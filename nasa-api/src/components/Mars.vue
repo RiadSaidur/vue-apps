8d14ae28e349790082877d2b3f38b6c99376be44
deathcrush <reidvai1998@gmail.com>
2019-09-30 19:21:10 +0600

    added next-prev feature for gallery

--- nasa-api/src/components/Mars.vue
+++ nasa-api/src/components/Mars.vue
@@ -1,5 +1,18 @@
 <template>
 <v-container>
+
+  <v-toolbar dense flat class="white">
+    <v-btn @click="previous">
+      <v-icon left>mdi-chevron-left</v-icon>
+      <span>Previous</span>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn @click="next">
+      <span>Next</span>
+      <v-icon right>mdi-chevron-right</v-icon>
+    </v-btn>
+  </v-toolbar>
+
   <v-layout row wrap>
 
     <v-flex xs12 md4 v-for="(photo, index) in photos" :key="index" class="pa-2">
@@ -8,7 +21,7 @@
 
       <v-img :src="photo.source" class="ma-auto my-2" max-width="500"></v-img>
       
-      <v-card-text> {{ photo.date }} <br> Rover: {{ photo.rover }} </v-card-text>
+      <v-card-text> {{ photo.date }} <br> Rover: {{ photo.rover }}  </v-card-text>
 
       <v-btn :href="photo.source" target="blank">Full Size</v-btn>
 
@@ -30,13 +43,26 @@ export default {
     return{
       date: 1000,
       apiKey: 'jRfHYNpuR6AhsBJswdaTk4PhBLlpIJfH8vfHL7XH',
-      manifest: `https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity?api_key=jRfHYNpuR6AhsBJswdaTk4PhBLlpIJfH8vfHL7XH`,
-      mars: `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos`,
+      page: 1,
+      manifest: `https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=jRfHYNpuR6AhsBJswdaTk4PhBLlpIJfH8vfHL7XH`,
+      mars: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos`,
       photos: []
     }
   },
 
   methods: {
+
+    next(){
+      this.page++;
+      this.getPhotos();
+    },
+
+    previous(){
+      if(this.page == 1) return;
+      this.page--;
+      this.getPhotos();
+    },
+
     getPhotos(){
       axios.get(this.manifest)
       .then(response => {
@@ -45,11 +71,14 @@ export default {
       })
       .then(()=>{
         
-        const marsURL = `${this.mars}?sol=${this.date}&api_key=${this.apiKey}`;
+        const marsURL = `${this.mars}?sol=${this.date}&page=${this.page}&api_key=${this.apiKey}`;
 
         axios.get(marsURL)
         .then(response => {
+          this.photos = [];
+
           let data = response.data.photos;
+
           const length = data.length;
           for(let i=0; i<length; i++){
             const photo = {
@@ -60,7 +89,6 @@ export default {
             this.photos.push(photo);
           }
         });
-
       });
     }
   },
