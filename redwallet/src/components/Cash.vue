adb61dcd2dd607fb4080ba425ea906eeee57dd48
deathcrush <reidvai1998@gmail.com>
2019-10-11 21:12:10 +0600

    added: Cash & Log components

---
+++ redwallet/src/components/Cash.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-card dark>
+      <v-card-text>Current Cash: {{cash}} BDT</v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Cash',
+  computed:{
+    ...mapState([
+      'cash'
+    ])
+    // cash(){
+    //   return this.$store.state.cash;
+    // }
+  },
+  methods:{
+    
+  }
+};
+</script>
