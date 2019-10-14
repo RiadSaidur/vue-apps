d7f9d7de69da77dd68a4355ea965a84d3f6aec09
deathcrush <reidvai1998@gmail.com>
2019-10-14 12:09:17 +0600

    added: profile view

---
+++ redwallet/src/views/Profile.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-conatiner>
+    <v-card class="mx-auto" height="100%" max-width="500" dark>
+      <v-list>
+        <v-list-item-group v-for="(items, index) in menuItems" :key='index'>
+          <router-link :to='items.link'>
+            <v-list-item>
+              <v-list-item-icon><v-icon>{{items.icon}}</v-icon></v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{items.title}}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </v-conatiner>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data(){
+    return{
+      menuItems:[
+        {
+          link: '/settings',
+          icon: 'mdi-settings-outline',
+          title: 'Settings'
+        },
+        {
+          link: '/settings',
+          icon: 'mdi-settings-outline',
+          title: 'Settings'
+        },
+        {
+          link: '/settings',
+          icon: 'mdi-settings-outline',
+          title: 'Settings'
+        }
+      ]
+    }
+  }
+}
+</script>
