3bdb8e2c08c3f86375e8195a5686b13ed737410b
deathcrush <reidvai1998@gmail.com>
2019-10-18 02:10:01 +0600

    updated: store mutations and actions

--- redwallet/src/store.js
+++ redwallet/src/store.js
@@ -5,54 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    cash: 7000,
-    income: 5000,
-    budget: 3000,
-    savingTarget: 2000,
-    logs:[
-      {
-        type: 'spend',
-        title: 'books',
-        amount: 300,
-        date:"14/9/2019"
-      },
-      {
-        type: 'income',
-        title: 'salary',
-        amount: 5500,
-        date:"14/9/2019"
-      },
-      {
-        type: 'spend',
-        title: 'shoes',
-        amount: 2000,
-        date:"27/6/2029"
-      },
-      {
-        type: 'income',
-        title: 'dhanda',
-        amount: 500,
-        date:"14/9/2019"
-      },
-      {
-        type: 'spend',
-        title: 'coke',
-        amount: 500,
-        date:"27/6/2029"
-      },
-      {
-        type: 'spend',
-        title: 'cake',
-        amount: 2500,
-        date:"27/6/2019"
-      },
-      {
-        type: 'income',
-        title: 'dhanda again',
-        amount: 900,
-        date:"14/9/2019"
-      },
-    ]
+    tab: '',
+    current:{
+      cash: 0,
+      income: 0,
+      budget: 0,
+      savingTarget: 0
+    },
+    logs:[]
   },
   getters:{
     today: state => {
@@ -61,24 +21,42 @@ export default new Vuex.Store({
       return state.logs.filter(log => {
         return log.date === date;
       })
+    },
+    allTransactions: state => {
+      return state.logs;
     }
   },
   mutations: {
     ADD_RECORD: (state, record) => {
-      state.logs.push(record);
-      if(record.type === 'income') state.cash += record.amount;
-      else if(record.type === 'spend') state.cash -= record.amount;
+      state.logs.unshift(record);
+      if(record.type === 'income') state.current.cash += record.amount;
+      else if(record.type === 'spend') state.current.cash -= record.amount;
     },
     UPDATE: (state, collection) => {
-      state.cash = collection.cash;
-      state.income = collection.income;
-      state.budget = collection.budget;
-      state.savingTarget = collection.target;
+      state.current.cash = collection.cash;
+      state.current.income = collection.income;
+      state.current.budget = collection.budget;
+      state.current.savingTarget = collection.target;
+    },
+    CHANGE_TAB: (state, title) => {
+      state.tab = title;
+    },
+    DELETE: (state, item) => {
+      const idx = state.logs.findIndex(log =>{
+        return log.title === item;
+      });
+      state.logs.splice(idx, 1);
     }
   },
   actions: {
     setData: (context, collection) => {
       context.commit('UPDATE', collection);
+    },
+    setTitle: (context, title) => {
+      context.commit('CHANGE_TAB', title);
+    },
+    deleteData: (context, item) => {
+      context.commit('DELETE', item);
     }
   }
 })
