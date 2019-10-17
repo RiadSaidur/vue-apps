1c0579c64fa3044e3fd7da54d095a0ab15dc45bb
deathcrush <reidvai1998@gmail.com>
2019-10-17 16:12:19 +0600

    added: dynamic heading

---
+++ redwallet/src/components/Heading.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar-title class="headline text-uppercase">
+    <router-link to='/'>
+      <span class="red--text">RED</span>
+      <span class="font-weight-light white--text">{{tab}}</span>
+    </router-link>
+  </v-toolbar-title>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  name: 'Heading',
+  computed:{
+    ...mapState([
+      'tab'
+    ])
+  }
+}
+</script>
