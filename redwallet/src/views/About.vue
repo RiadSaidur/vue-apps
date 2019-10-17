f564500fbaec5df79c08c64cc1f73549d80c29ac
deathcrush <reidvai1998@gmail.com>
2019-10-18 02:10:25 +0600

    created: about page

--- redwallet/src/views/About.vue
+++ redwallet/src/views/About.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-card dark height='50vh' width='720' class="ma-auto">
+      <v-card-title dark>WALLET APP</v-card-title>
+      <v-card-text>This Wallet App is created using:</v-card-text>
+      <v-card-text v-for="(tech, idx) in techs" :key="idx" class="mb-0 pb-0">## {{tech}}</v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 import { mapMutations } from 'vuex'
 
 export default {
+  name: 'About',
+  data(){
+    return{
+      techs: [
+        'Vue 2',
+        'Vue-Router',
+        'Vuex',
+        'Vuetify'
+      ]
+    }
+  },
   methods: {
     ...mapMutations([
       'CHANGE_TAB'
